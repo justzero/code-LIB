892696ad594780e63830dd04ee6b9394a7e6f74f
justzero <gb.justzero@gmail.com>
2011-09-27 11:44:12 +0800

    del all!

--- test.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var file = 'readme.txt';
-
-fs.readFile(file, 'utf8', function(err, data) {
-	if (err) throw err;
-	console.log(data);
-	console.log("hello gits!");
-});
-oconsole.log('this is node js!');
